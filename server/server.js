4f9f601e5a60be8b464762415ca10216528eb4ff
Kanchan-ai05 <shawkanchan580@gmail.com>
2026-09-12 22:43:24 +0530

    Add changes

--- server/server.js
+++ server/server.js
@@ -127,7 +127,7 @@ app.use((req, res, next) => {
 // Error handling middleware
 app.use(errorHandler);
 
-const MONGODB_URI = process.env.MONGO_URI;
+const MONGODB_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/liferpg'
 
 mongoose
   .connect(MONGODB_URI)
